d4c8a5a7b6fe6eb10fc8b47d133bf55579465880
Andrew Deck <adeck@users.noreply.github.com>
2024-01-12 17:31:08 -0800

    Building a little app to display movies (#2)
    * followed tutorial from: https://www.youtube.com/watch?v=b9eMGE7QtTk

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,68 @@
 import React from 'react';
-import logo from './logo.svg';
+import {useState} from 'react';
 import './App.css';
+import SearchIcon from './search.svg';
+import {API_TOKEN} from "./ApiSecret";
+import MovieCard from "./MovieCard";
 
+// Following this tutorial:
+//
+//  https://www.youtube.com/watch?v=b9eMGE7QtTk
+//
+
+const API_URL: string = `https://www.omdbapi.com?apikey=${API_TOKEN}`;
+
+//function App() {
+//  return (
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload. Or not.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [movies, setMovies] = useState([]);
+    const [searchTerm, setSearchTerm] = useState('');
+    const searchMovies = async (title: string) => {
+        // encodeURIComponent is not in the original tutorial, but
+        //  seemed too important to sanitize user input here. Obviously, this
+        //  is a toy application, but still.
+        const response = await fetch(`${API_URL}&s=${encodeURIComponent(title)}`);
+        const data = await response.json();
+        // noinspection JSUnresolvedReference
+        const moviesResp = data?.Search;
+        if (moviesResp?.length > 0) {
+            setMovies(moviesResp);
+        }
+        console.log(moviesResp);
+    }
+    /* So, FilmCow is a play on words. Which I just got, lol. */
+    return (
+        <div className='App'>
+            <h1>CinemaBoeuf</h1>
+            <div className={'search'}>
+                <input
+                    placeholder={'Search'}
+                    value={searchTerm}
+                    onChange={(e) => {
+                        setSearchTerm(e.target.value);
+                    }}
+                />
+                <img
+                    src={SearchIcon}
+                    alt={'search'}
+                    onClick={() => searchMovies(searchTerm)}
+                />
+            </div>
+            {
+                movies?.length > 0 ?
+                    (<div className={'container'}>
+                        {movies.map((movie) => (
+                            <MovieCard movie={movie}/>
+                        ))}
+                    </div>)
+                    : (
+                        <div className={"empty"}>
+                            <h2>No movies found.</h2>
+                        </div>
+                    )
+            }
+        </div>
+    );
 }
 
 export default App;

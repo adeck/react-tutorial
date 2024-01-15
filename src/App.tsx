8ea413a5182360eb593294a3b654be05973cabc4
adeck <adeck@users.noreply.github.com>
2024-01-14 21:55:42 -0800

    Currently renders all the calendars for the year.

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,98 @@
 import React from 'react';
 import {useState} from 'react';
 import './App.css';
-import SearchIcon from './search.svg';
-import {API_TOKEN} from "./ApiSecret";
-import MovieCard from "./MovieCard";
 
-// Following this tutorial:
-//
-//  https://www.youtube.com/watch?v=b9eMGE7QtTk
-//
-
-const API_URL: string = `https://www.omdbapi.com?apikey=${API_TOKEN}`;
-
-//function App() {
-//  return (
 function App() {
-    const [movies, setMovies] = useState([]);
-    const [searchTerm, setSearchTerm] = useState('');
-    const searchMovies = async (title: string) => {
-        // encodeURIComponent is not in the original tutorial, but
-        //  seemed too important to sanitize user input here. Obviously, this
-        //  is a toy application, but still.
-        const response = await fetch(`${API_URL}&s=${encodeURIComponent(title)}`);
-        const data = await response.json();
-        // noinspection JSUnresolvedReference
-        const moviesResp = data?.Search;
-        if (moviesResp?.length > 0) {
-            setMovies(moviesResp);
-        }
-        console.log(moviesResp);
-    }
-    /* So, FilmCow is a play on words. Which I just got, lol. */
+    const today = new Date();
+    const [year, setYear] = useState(today.getFullYear());
+    // The month value is zero-indexed; January is 0, December is 11.
+    const [monthZeroIdx, setMonthZeroIdx] = useState(today.getMonth());
+    return (<div className={"App"}>
+        {Array.from(Array(12).keys()).map((monthIdx) => (
+            <Month key={monthIdx} year={year} monthZeroIdx={monthIdx} />
+        ))}
+    </div>);
+    //         <Month year={year} monthZeroIdx={monthZeroIdx} />
+}
+
+interface MonthId {
+    year: number,
+    monthZeroIdx: number,
+}
+function Month({year, monthZeroIdx}: MonthId) {
+    return (<table>
+        <MonthHeader year={year} monthZeroIdx={monthZeroIdx} />
+        <MonthBody year={year} monthZeroIdx={monthZeroIdx}/>
+    </table>);
+}
+function MonthHeader({year, monthZeroIdx}: MonthId) {
     return (
-        <div className='App'>
-            <h1>CinemaBoeuf</h1>
-            <div className={'search'}>
-                <input
-                    placeholder={'Search'}
-                    value={searchTerm}
-                    onChange={(e) => {
-                        setSearchTerm(e.target.value);
-                    }}
-                />
-                <img
-                    src={SearchIcon}
-                    alt={'search'}
-                    onClick={() => searchMovies(searchTerm)}
-                />
-            </div>
-            {
-                movies?.length > 0 ?
-                    (<div className={'container'}>
-                        {movies.map((movie) => (
-                            <MovieCard movie={movie}/>
-                        ))}
-                    </div>)
-                    : (
-                        <div className={"empty"}>
-                            <h2>No movies found.</h2>
-                        </div>
-                    )
-            }
-        </div>
+        <thead>
+            <tr>
+                <th colSpan={7}>{year} {getMonthName(monthZeroIdx)}</th>
+            </tr>
+            <tr>{['M', 'T', 'W', 'Th', 'F', 'Sa', 'Su'].map((e, i) => (
+                <th key={i}>{e}</th>
+            ))}</tr>
+        </thead>
     );
+}
+
+const daysInMonth = ({year, monthZeroIdx} : MonthId) => new Date(year, monthZeroIdx + 1, 0).getDate();
+
+function MonthBody({year, monthZeroIdx} : MonthId) {
+    return (
+        <tbody>
+            {getMonthDayIndices({year, monthZeroIdx}).map(
+                (dayIdxArr, idx) =>
+                    (<Week key={idx} year={year} monthZeroIdx={monthZeroIdx} days={dayIdxArr}/>))
+            }
+        </tbody>
+    );
+}
+
+interface WeekProps {
+    year: number,
+    monthZeroIdx: number,
+    days: number[],
+}
+const Week = ({year, monthZeroIdx, days}: WeekProps) => {
+    return (
+      <tr>
+          {days.map((day, idx) => (<Day key={idx} year={year} monthZeroIdx={monthZeroIdx} day={day}/>))}
+      </tr>
+    );
+}
+
+interface DayProps {
+    year: number,
+    monthZeroIdx: number,
+    day: number,
+}
+const Day = ({year, monthZeroIdx, day}: DayProps) => (<td>{day}</td>);
+
+function getMonthName(monthZeroIdx: number) {
+    const tmpDate = new Date(`2020-${monthZeroIdx + 1}-1`);
+    return new Intl.DateTimeFormat("en-US",
+        { month: "long" }).format(tmpDate);
+}
+
+function getMonthDayIndices({year, monthZeroIdx} : MonthId) : number[][] {
+    const numDaysInMonth = daysInMonth({year, monthZeroIdx});
+    const firstWeekPaddingLength = new Date(year, monthZeroIdx, 1).getDay();
+    const lastWeekPaddingLength = (7 - (firstWeekPaddingLength + numDaysInMonth) % 7) % 7;
+    console.log(`${firstWeekPaddingLength} ${numDaysInMonth} ${lastWeekPaddingLength}`);
+    const padding = (length: number) => Array(length).fill(-1);
+    const dayIdxArray = [
+        ...padding(firstWeekPaddingLength),
+        ...Array.from(Array(numDaysInMonth).keys()).map((n) => n + 1),
+        ...padding(lastWeekPaddingLength),
+    ];
+    const weeks : number[][] = [];
+    for (let i = 0; i < dayIdxArray.length / 7; i++) {
+        weeks.push(dayIdxArray.slice(i * 7, i * 7 + 7));
+    }
+    return weeks;
 }
 
 export default App;
